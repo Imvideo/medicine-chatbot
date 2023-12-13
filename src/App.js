bd15b615324ddfc7a0cd0c14b868110fab905569
Jeon Young Sang <iwtl0303@naver.com>
2023-12-14 00:41:07 +0900

    version4

--- src/App.js
+++ src/App.js
@@ -11,38 +11,100 @@ function App() {
   const [chatBubbleBot, setBubbleBot] = useState([]);
   const [chatBubbleChat, setBubbleChat] = useState([]);
   const [dataFromSpring, setDataFromSpring] = useState('');
-  const [backendResponse, setBackendResponse] = useState(null);
-  const buttons = ['두통', '메스꺼움', '복통', '기침', '어지러움', '치통', '생리통'];
-
+  const [backendResponse, setBackendResponse] = useState([]);
+  const buttons = ['두통', '메스꺼움', '복통', '기침', '어지러움', '치통'];
+  
   const requestData = {
     userMessage: 'yourUsername'
   };
   
-
-  const sendToBackend = async (data) => {
-    fetch('http://34.204.171.30:8080/askLexV2', {
-      method: 'POST',
+  const sendGetRequest = (text) => {
+    fetch(`http://ec2-52-203-48-52.compute-1.amazonaws.com:8080/api/requests`, {
+      method: 'GET',
       headers: {
-        'Content-Type': 'application/json', // 데이터 형식을 JSON으로 지정
-        // 다른 필요한 헤더도 여기에 추가할 수 있습니다.
-      },
-      body: JSON.stringify(requestData) // 데이터를 JSON 문자열로 변환하여 body에 추가
+        'Content-Type': 'application/json',
+      }
     })
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        return response.json(); // 응답 데이터를 JSON 형식으로 파싱
+        return response.json();
       })
       .then(data => {
-        console.log('Success:', data);
-        // 서버 응답에 대한 처리
+        console.log('Received Data:', data);
+        // const newBubbleUser = {
+        //   text: text,
+        // };
+        // setBubbleUser([...chatBubbleUser, newBubbleUser]);
+        const newBackendResponse = {
+          data : data,
+        };
+        setBackendResponse([...backendResponse, newBackendResponse]);
+        console.log('newBackendResponse 타입:', typeof newBackendResponse);
+
+
+      
+        //setBackendResponse(data);
+        // 받은 데이터 처리
       })
       .catch(error => {
         console.error('Error:', error);
         // 에러 처리
       });
-    };    
+  };
+
+  // const sendToBackend = async (data) => {
+  //   requestData.userMessage = data;
+  //   fetch('http://34.204.171.30:8080/askLexV2', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json', // 데이터 형식을 JSON으로 지정
+  //       // 다른 필요한 헤더도 여기에 추가할 수 있습니다.
+  //     },
+  //     body: JSON.stringify(requestData) // 데이터를 JSON 문자열로 변환하여 body에 추가
+  //   })
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //       return response.json(); // 응답 데이터를 JSON 형식으로 파싱
+  //     })
+  //     .then(data => {
+  //       console.log('Success:', data);
+  //       setTimeout(sendGetRequest(),4000); // 1초 후에 delayedAction 함수 실행
+  //       ;
+  //       // 서버 응답에 대한 처리
+  //     })
+  //     .catch(error => {
+  //       console.error('Error:', error);
+  //       // 에러 처리
+  //     });
+  //   };    
+  const sendToBackend = async (data) => {
+    requestData.userMessage = data;
+  
+    try {
+      const response = await fetch('http://ec2-52-203-48-52.compute-1.amazonaws.com:8080/askLexV2', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData)
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      console.log('Success:', responseData);
+      await sendGetRequest(); // 이 부분이 비동기적으로 처리됩니다.
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
 
   const handleButtonClick = (text) => {
     addNewBubbleUser(text); // 사용자쪽 말풍선
@@ -55,6 +117,7 @@ function App() {
       text: text,
     };
     setBubbleUser([...chatBubbleUser, newBubbleUser]);
+    
   };
 
   const addNewBubbleBot = (text) => {
@@ -79,12 +142,13 @@ function App() {
   const handleInputEnter = (e) => {
     if (e.key === 'Enter') {
       const userInput = e.target.value;
-      addNewBubbleUser(userInput);
+      addNewBubbleChat(userInput);
       sendToBackend(userInput);
       e.target.value = ''; // 입력란 비우기
     }
   };
 
+  
   return (
     <div className="center-container">
       <div className="subcenter-container">
@@ -95,7 +159,7 @@ function App() {
         <input type="text" placeholder="증상을 입력해 주세요." onKeyPress={handleInputEnter} />
         <div className="square-container">
           <ChatBubble text="어디가 아프신가요?" buttons={buttons} onClick={handleButtonClick} />
-          {chatBubbleUser.map((bubble, index) => (
+          {backendResponse && chatBubbleUser.map((bubble, index) => (
             <React.Fragment key={index}>
               <ChatBubbleUser
                 key={'user-' + index}
@@ -104,10 +168,15 @@ function App() {
               />
                 <TextChatBubbleBot
       key={'bot-' + chatBubbleBot.length + index} 
-      text={chatBubbleBot[index] ? chatBubbleBot[index].text : ''} 
+      text={backendResponse[index] ? `${backendResponse[index].data.symptom}에 대한 약을 추천해 드리겠습니다.\n 
+      약 이름은 ${backendResponse[index].data.itemName}입니다.
+      약의 효능은 다음과 같습니다. ${backendResponse[index].data.effect}
+      부작용은 다음과 같습니다. ${backendResponse[index].data.sideEffect}` : ' '} 
+
       addNewBubble={addNewBubbleBot}
     />
-            </React.Fragment>
+    <ChatBubble text="어디가 아프신가요?" buttons={buttons} onClick={handleButtonClick} />
+  </React.Fragment>
           ))}
         </div>
       </div>
--- src/TextChatBubbleBot.js
+++ src/TextChatBubbleBot.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ChatBubble.css';
 
-function TextChatBubbleBot ({ text })   {
-    return (
-      <div className="chat-bubble">
-        <div className="text">{text}</div>
-        <div className="bubble-tail-user"></div>
-      </div>
-    );
-  };
+function TextChatBubbleBot ({ text }) {
+  const [delayedText, setDelayedText] = useState('');
 
-  export default TextChatBubbleBot;
+  useEffect(() => {
+    // 1초 후에 text 값을 delayedText에 설정
+    const timer = setTimeout(() => {
+      setDelayedText(text);
+    }, 1000);
+
+    return () => clearTimeout(timer); // 컴포넌트가 unmount되기 전에 타이머를 정리
+  }, [text]);
+
+  return (
+    <div className="chat-bubble">
+      <div className="text">{delayedText}</div>
+      <div className="bubble-tail-user"></div>
+    </div>
+  );
+};
+
+export default TextChatBubbleBot;
